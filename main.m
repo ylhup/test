95ce55a6593ded048368073b20b4561188d7c19f
huyanliang <huyanliang@qgutech.com>
2020-04-18 15:03:36 +0800

    frisi commit

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Qgutech_ios
+//
+//  Created by huyanliang on 2018/7/10.
+//  Copyright © 2018年 qgutech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "QguAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QguAppDelegate class]));
+    }
+}
